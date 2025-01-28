f650216d035f6df352933c1c315cb465d364610c
Frizlab <fload@me.com>
2025-01-28 21:33:31 +0100

    Add support for Swift 5.1

--- Package@swift-5.8.swift
+++ Package@swift-5.8.swift
@@ -1,9 +1,18 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.8
 import PackageDescription
 
 
+//let swiftSettings: [SwiftSetting] = []
+let swiftSettings: [SwiftSetting] = [.enableExperimentalFeature("StrictConcurrency")]
+
 let package = Package(
 	name: "json-logger",
+	platforms: [
+		.macOS(.v11),
+		.tvOS(.v14),
+		.iOS(.v14),
+		.watchOS(.v7),
+	],
 	products: [
 		.library(name: "JSONLogger", targets: ["JSONLogger"])
 	],
@@ -15,7 +24,7 @@ let package = Package(
 		.target(name: "JSONLogger", dependencies: [
 			.product(name: "GenericJSON",   package: "generic-json"),
 			.product(name: "Logging",       package: "swift-log"),
-		], path: "Sources"),
-		.testTarget(name: "JSONLoggerTests", dependencies: ["JSONLogger"]),
+		], path: "Sources", swiftSettings: swiftSettings),
+		.testTarget(name: "JSONLoggerTests", dependencies: ["JSONLogger"], swiftSettings: swiftSettings),
 	]
 )
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,43 @@
+import Foundation
+import XCTest
+
+@testable import JSONLoggerTests
+
+
+
+var tests: [XCTestCaseEntry] = [
+	testCase([
+		("test0NoSeparatorForFirstLog", JSONLoggerTests.test0NoSeparatorForFirstLog),
+		("testFromDoc", JSONLoggerTests.testFromDoc),
+		("testSeparatorForNotFirstLog", JSONLoggerTests.testSeparatorForNotFirstLog),
+		("testEncodeMetadataAsJSON", JSONLoggerTests.testEncodeMetadataAsJSON),
+		("testFallbackOnLogLineEncodeFailure", JSONLoggerTests.testFallbackOnLogLineEncodeFailure),
+		("testDecodeLogLineWithBothValidDateAndMangledDate", JSONLoggerTests.testDecodeLogLineWithBothValidDateAndMangledDate),
+		("testDecodeLogLineWithBothInvalidDateAndMangledDate", JSONLoggerTests.testDecodeLogLineWithBothInvalidDateAndMangledDate),
+	]),
+]
+#if !os(WASI)
+XCTMain(tests)
+
+#else
+/* Compilation fails for Swift <5.5… */
+//await XCTMain(tests)
+
+/* Let’s print a message to inform the tests on WASI are disabled. */
+let brightRed = "\u{1B}[91;1m"
+let gray = "\u{1B}[38;5;245m"
+let magenta = "\u{1B}[35;1m"
+let reset = "\u{1B}[0m"
+try FileHandle.standardError.write(contentsOf: Data("""
+\(brightRed)Tests are disabled on WASI\(reset):
+\(gray)This package is compatible with Swift <5.4, so we have to add a LinuxMain file in which we call XCTMain.
+On WASI the XCTMain function is async, so we have to #if the XCTMain call, one with the await keyword, the other without.
+However, on Swift <5.5 the LinuxMain setup like this does not compile because the old compiler does not know the await keyword
+ (even though the whole code is ignored because we do not compile for WASI whe compiling with an old compiler).
+I also tried doing a #if swift(>=5.5) check, but that do not work either.\(reset)
+
+\(magenta)To temporarily enable the tests for WASI, uncomment the `await XCTMain(tests)` line in LinuxMain.swift.\(reset)
+
+""".utf8))
+
+#endif
