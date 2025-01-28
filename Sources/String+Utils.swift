9ad0b92051a8680c01ef3a9382e5f6c5b563ec7d
Frizlab <fload@me.com>
2025-01-28 21:44:41 +0100

    Fix usage of #""# for pre-Swift 5.4 compilers

--- Sources/String+Utils.swift
+++ Sources/String+Utils.swift
@@ -6,12 +6,13 @@ internal extension String {
 	
 	func safifyForJSON() -> String {
 		let ascii = unicodeScalars.lazy.map{ scalar in
+			/* Note: The #""# syntax is available starting at Swift 5.4. */
 			switch scalar {
 				case _ where !scalar.isASCII: return "-"
-				case #"\"#: return #"\\"#
-				case #"""#: return #"\""#
-				case "\n": return #"\n"#
-				case "\r": return #"\r"#
+				case "\\": return "\\\\"
+				case "\"": return "\\\""
+				case "\n": return "\\n"
+				case "\r": return "\\r"
 				/* `scalar.value` should never be bigger than Int32.max, but we still use bitPattern not to crash if it is. */
 				case _ where isprint(Int32(bitPattern: scalar.value)) == 0: return "-"
 				default: return String(scalar)
