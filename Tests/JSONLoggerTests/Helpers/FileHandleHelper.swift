b238331d6439ced395a7ff8c6d15a32e70cf6fa8
Frizlab <fload@me.com>
2025-09-24 00:36:38 +0200

    Fix compilation of the test target on Windows

--- Tests/JSONLoggerTests/Helpers/FileHandleHelper.swift
+++ Tests/JSONLoggerTests/Helpers/FileHandleHelper.swift
@@ -19,11 +19,15 @@ extension FileHandle {
 		if #available(macOS 10.15, *) {
 			try close()
 		} else {
+#if !os(Windows)
 			/* closeFile exists but I’m not sure it never throws an ObjC exception, so I call the C function. */
 			let ret = system_close(fileDescriptor)
 			guard ret == 0 else {
 				throw Errno()
 			}
+#else
+			fatalError("Unreachable code reached: In else of #available with only Apple platforms checks, but also on Windows.")
+#endif
 		}
 	}
 	
@@ -31,45 +35,39 @@ extension FileHandle {
 		if #available(macOS 10.15, tvOS 13.0, iOS 13.0, watchOS 6.0, *) {
 #if swift(>=5.2) || !canImport(Darwin)
 			return try readToEnd()
-#else
+#elseif !os(Windows)
 			let buffer = UnsafeMutableRawBufferPointer.allocate(byteCount: 512, alignment: MemoryLayout<UInt8>.alignment)
 			defer {buffer.deallocate()}
 			
-#if !os(Windows)
 			var nread = 0
-			let bufferCount = buffer.count
-#else
-			var nread = Int32(0)
-			let bufferCount = UInt32(buffer.count)
-#endif
 			var ret = Data()
-			while ({ nread = system_read(fileDescriptor, buffer.baseAddress, bufferCount); return nread }()) > 0 {
+			while ({ nread = system_read(fileDescriptor, buffer.baseAddress, buffer.count); return nread }()) > 0 {
 				ret += buffer[0..<Int(nread)]
 			}
 			guard nread >= 0 else {
 				throw Errno()
 			}
 			return ret
+#else
+ #error("How can we get here? We can import Darwin, but we are on Windows??")
 #endif
 		} else {
+#if !os(Windows)
 			let buffer = UnsafeMutableRawBufferPointer.allocate(byteCount: 512, alignment: MemoryLayout<UInt8>.alignment)
 			defer {buffer.deallocate()}
 			
-#if !os(Windows)
 			var nread = 0
-			let bufferCount = buffer.count
-#else
-			var nread = Int32(0)
-			let bufferCount = UInt32(buffer.count)
-#endif
 			var ret = Data()
-			while ({ nread = system_read(fileDescriptor, buffer.baseAddress, bufferCount); return nread }()) > 0 {
+			while ({ nread = system_read(fileDescriptor, buffer.baseAddress, buffer.count); return nread }()) > 0 {
 				ret += buffer[0..<Int(nread)]
 			}
 			guard nread >= 0 else {
 				throw Errno()
 			}
 			return ret
+#else
+			fatalError("Unreachable code reached: In else of #available with only Apple platforms checks, but also on Windows.")
+#endif
 		}
 	}
 	
